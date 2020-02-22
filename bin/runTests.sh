c8f0eba667abf1fe1d0b9830c43b79e8a38f7923
Anthony Gauthier <antho325@hotmail.com>
2020-02-22 00:57:33 -0500

    Fixed router unit tests

--- bin/runTests.sh
+++ bin/runTests.sh
@@ -3,4 +3,4 @@ clear
 deno tests/unit/Middleware_Test.ts 
 deno tests/unit/Response_Test.ts 
 deno tests/unit/Route_Test.ts 
-deno tests/unit/Router_Test.ts 
+deno --allow-net tests/unit/Router_Test.ts 
--- tests/unit/Router_Test.ts
+++ tests/unit/Router_Test.ts
@@ -1,7 +1,9 @@
 import { assertEquals, unimplemented } from "https://deno.land/std/testing/asserts.ts";
+import { serve } from "https://deno.land/std/http/server.ts";
 import { Router } from "../../core/Router.ts";
 
-const router = new Router("/test", "GET");
+const server = serve({port: 4000});
+const router = new Router(server);
 
 Deno.test({
     name: "Testing Router construct",
